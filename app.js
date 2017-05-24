ffaa55e12dfc7e57362a2cb5b27717598d317d45
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-05-24 21:27:19 +0300

    started with io

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-var functions = require("./functions")
-var objects = require("./objects")
-var kdTreeCreator = require("static-kdtree")
+var functions = require("./functions");
+var objects = require("./objects");
+var kdTreeCreator = require("static-kdtree");
+// todo csv dep
+var fs = require("fs");
+
+function Input(stations, points) {
+    this.stationsPath = stations;
+    this.pointsPath = points;
+}
+
+Input.prototype.readStations = function() {
+
+}
+
+function Ouptut(out) {
+    this.outPath = out;
+}
+
 
 // todo read stations from somewhere
 // todo read points from somewhere
