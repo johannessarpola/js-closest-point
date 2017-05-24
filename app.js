856b4c553b30930828d6864bc14fb072acc97078
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-05-24 17:22:29 +0300

    todo skeches

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var functions = require("./functions")
 var objects = require("./objects")
 var kdTreeCreator = require("static-kdtree")
+
+// todo read stations from somewhere
+// todo read points from somewhere
+// todo output results for best points
